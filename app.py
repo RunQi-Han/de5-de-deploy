81a82dbcd83e6a6ecf7925c75fcc286afe288ca1
Runqi Han <77292603+RunQi-Han@users.noreply.github.com>
2026-04-09 14:57:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,86 @@
-from dash import Dash, dcc
 import dash_design_kit as ddk
+import numpy as np
 import plotly.express as px
+from dash import Dash, Input, Output, callback, dcc
 
 app = Dash(__name__)
-server = app.server  # expose server variable for Procfile
+server = app.server
 
 
-controls = [
-    ddk.ControlItem(
-        dcc.Dropdown(['Rear', 'Front', 'Side'], ['Rear'], multi=True),
-        label='Engine',
-    ),
-    ddk.ControlItem(
-        dcc.Slider(0, 10,
-            marks={
-                0: '0',
-                3: '3',
-                5: '5',
-                7.65: '7.65 °F',
-                10: '10'
-            },
-            value=5
+df = px.data.gapminder()
+
+app.layout = ddk.App(
+    [
+        ddk.Header(
+            [
+                ddk.Logo(src=app.get_asset_url("logo.png")),
+                ddk.Title("GoPro Test 2"),
+            ]
         ),
-        label='Thrusters',
-    ),
-    ddk.ControlItem(
-        dcc.Input(
-            value=50,
-            type='number'
+        ddk.ControlCard(
+            [
+                ddk.ControlItem(
+                    dcc.Dropdown(
+                        df["country"].unique(),
+                        ["Canada", "Panama", "Norway", "France"],
+                        multi=True,
+                        id="dropdown-country",
+                    ),
+                    label="Country",
+                ),
+                ddk.ControlItem(
+                    dcc.RangeSlider(
+                        min=df["year"].min(),
+                        max=df["year"].max(),
+                        marks={
+                            int(year): str(year) for year in sorted(df["year"].unique())
+                        },
+                        value=[df["year"].min(), df["year"].max()],
+                        step=None,
+                        id="slider",
+                    ),
+                    label="Year Range",
+                ),
+                ddk.ControlItem(
+                    dcc.Dropdown(
+                        options={
+                            "lifeExp": "Life Expectancy",
+                            "pop": "Population",
+                            "gdpPercap": "GDP per Capita",
+                        },
+                        value="pop",
+                        id="dropdown-y-axis",
+                    ),
+                    label="Y-axis Variable",
+                ),
+            ],
+            orientation="horizontal",
         ),
-        label='Power',
+        ddk.Card(
+            children=ddk.Graph(id="graph"),
+        ),
+    ],
+    show_editor=True,
+)
+
+
+@callback(
+    Output("graph", "figure"),
+    Input("dropdown-country", "value"),
+    Input("slider", "value"),
+    Input("dropdown-y-axis", "value"),
+)
+def update_graph(country, year_range, y_axis):
+    df = px.data.gapminder()
+    filtered_df = df[df["country"].isin(country)]
+    filtered_df = filtered_df[
+        (filtered_df["year"] >= year_range[0]) & (filtered_df["year"] <= year_range[1])
+    ]
+    figure = px.line(
+        filtered_df, x="year", y=y_axis, color="country", title=f"{y_axis} by Country"
     )
-]
-
-menu = ddk.Menu([
-    ddk.CollapsibleMenu(
-        title='Performance',
-        default_open=False,
-        children=[
-            dcc.Link('Team 1', href=app.get_relative_path('/')),
-            dcc.Link('Team 2', href=app.get_relative_path('/')),
-        ]
-    ),
-    dcc.Link('Conditions', href=app.get_relative_path('/')),
-    dcc.Link('Historical', href=app.get_relative_path('/')),
-    dcc.Link('Portal', href=app.get_relative_path('/')),
-])
-
-df = px.data.stocks()
-
-app.layout = ddk.App([
-
-    ddk.Header([
-        ddk.Logo(src=app.get_asset_url('logo.svg')),
-        ddk.Title('2026031222222'),
-        menu
-    ]),
-
-    ddk.ControlCard(controls, width=30),
-
-    ddk.Card(width=70, children=ddk.Graph(figure=px.line(df, x="date", y=["GOOG", "AAPL"], title='Stock Prices')))
-
-])
+    return figure
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=8050)
